c8115a5f4a7cc552327030eb6daf60424fb8208f
Zhack <zhackariya@gmail.com>
2015-10-28 03:54:27 +0800

    initial files upload

---
+++ iMicroServices.Web/Controllers/ServiceController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace iMicroServices.Web.Controllers
+{
+    public class ServiceController : Controller
+    {
+        // GET: Service
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult News()
+        {
+            return View();
+        }
+
+        public ActionResult HelpDesk()
+        {
+            return View();
+        }
+    }
+}
